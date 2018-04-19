b5b48ba4ae7a733feffbc6d229185006c2291ce4
ChangePan <change_pan@163.com>
2018-04-19 21:46:59 +0800

    添加注释

--- DouYuZB/DouYuZB/Classes/Tools/Extension/UIColor-Extension.swift
+++ DouYuZB/DouYuZB/Classes/Tools/Extension/UIColor-Extension.swift
@@ -10,6 +10,7 @@ import UIKit
 
 extension UIColor {
     
+    //MARK: - RGB设置颜色
     convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
         
         self.init(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: a)
